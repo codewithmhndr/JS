fe4d2be8fd8b5930df5ec07c77ce995bcd4a6c9b
codewithmhndr <codewithmhndr@gmail.com>
2025-09-28 20:56:19 +0530

    Completed Practise1

--- first.js
+++ first.js
@@ -1,3 +1,8 @@
-console.log("Hello JS!");
-fullName = "Tony Stark";
-console.log(fullName);
+const product = {
+    fullName :"Parker Jotter Standard CT Ball Pen(Black)",
+    Price : 270,
+    Rating : 4/5,
+    Offer : "50% Off"
+};
+
+console.log(product);
