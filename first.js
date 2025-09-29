51118571854393a241de4d9166a756d74df9887e
codewithmhndr <codewithmhndr@gmail.com>
2025-09-29 10:05:40 +0530

    Completed Second Part

--- first.js
+++ first.js
@@ -1,6 +1,12 @@
-let num = prompt("Enter Number:");
-if (num%5==0){
-    console.log("Number Is Multiple Of 5 !")
-} else{
-    console.log("Number Is Not Multiple Of 5!")
+let mark = prompt("Enter Your Marks") ;
+if (mark>=80){
+    console.log("Your Grade Is A");
+} else if(mark<80 && mark>=70){
+    console.log("Your Grade Is B");
+}else if(mark<70 && mark>=60){
+    console.log("Your Grade Is C");
+}else if(mark<60 && mark>=50){
+    console.log("Your Grade Is D");
+}else{
+    console.log("Your Grade Is F");
 }
