80fa8ac8cf40612bcc7acc6dacb482ca939bbd92
codewithmhndr <codewithmhndr@gmail.com>
2025-09-29 09:58:01 +0530

    Completed First Part

--- first.js
+++ first.js
@@ -1,3 +1,6 @@
-console.log("Hello JS!");
-fullName = "Tony Stark";
-console.log(fullName);
+let num = prompt("Enter Number:");
+if (num%5==0){
+    console.log("Number Is Multiple Of 5 !")
+} else{
+    console.log("Number Is Not Multiple Of 5!")
+}
