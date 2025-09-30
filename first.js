66f44c8a06edd98a55616c16a9271a4a51246138
codewithmhndr <codewithmhndr@gmail.com>
2025-09-30 16:34:04 +0530

    Complete First Part

--- first.js
+++ first.js
@@ -1,3 +1,8 @@
-console.log("Hello JS!");
-fullName = "Tony Stark";
-console.log(fullName);
+// Q1. Print All Even Numbers From 0 to 100
+
+console.log("| Even Numbers From 0 to 100 |");
+for (let num =0; num <=100; num ++){
+    if(num%2==0){
+        console.log(num);
+    }
+}
