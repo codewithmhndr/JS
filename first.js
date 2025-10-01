a2bca9754332858214fc3e06704ac71ffdbf4a7c
codewithmhndr <codewithmhndr@gmail.com>
2025-10-01 07:19:51 +0530

    Completed

--- first.js
+++ first.js
@@ -1,6 +1,9 @@
-let marks = [85,97,44,37,76,60];
-let avg =0;
-for (let i =0; i<marks.length;i++){
-    avg = avg+marks[i];
+let prices = [250,645,300,900,50];
+console.log("Prices Before Offer", prices);
+for (var val of prices){
+    let offer = val/10;
+    val = val-offer;
+    console.log("The Final Offers Is:",val);
+    
 };
-console.log("Average Marks :" ,(avg/marks.length));
+
