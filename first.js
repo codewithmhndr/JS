16324e5823a32ce18b4b140ab8e68a45708b1e8b
codewithmhndr <codewithmhndr@gmail.com>
2025-10-01 06:43:41 +0530

    Complete First Part

--- first.js
+++ first.js
@@ -1,7 +1,6 @@
-// Generate Username From Users Input
-
-
-let username = prompt("Enter Your Name");
-let len = username.length;
-let id = `@${username}${len}`;
-console.log(id);
+let marks = [85,97,44,37,76,60];
+let avg =0;
+for (let i =0; i<marks.length;i++){
+    avg = avg+marks[i];
+};
+console.log("Average Marks :" ,(avg/marks.length));
