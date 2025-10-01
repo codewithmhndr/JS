60cc1845121bdf362df500d6157c2b775161576f
codewithmhndr <codewithmhndr@gmail.com>
2025-10-01 07:52:09 +0530

    Completed Practise Set 6

--- first.js
+++ first.js
@@ -1,3 +1,13 @@
-console.log("Hello JS!");
-fullName = "Tony Stark";
-console.log(fullName);
+let company = ["Bloomberg", "Microsoft", "Uber", "Google", "IMB", "Netflix"];
+
+//  Removing First Company From Array
+
+console.log(company.splice(0,1));
+
+// Removing Uber And Adding Ola
+
+console.log(company.splice(1,1, "Ola"));
+
+// Adding Amazon At End
+
+console.log(company.push("Amazon"));
